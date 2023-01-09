129ca9b2fb4c241832c726216506da6532f1c95d
Jonathan Pohahau <jonathanpohahau@Jonathans-MBP.attlocal.net>
2023-01-08 20:47:18 -0800

    added local storage get item for each time block

--- Develop/script.js
+++ Develop/script.js
@@ -18,14 +18,14 @@ $(function () {
   // adding a listener to saveBtn with a function to set items in local storage
   $(".saveBtn").on("click", function() {
     // creating a variable description equaling the value content added by user from the description html class
-    var description = $(this).sibling(".description").val()
+    var description = $(this).append(".description").val()
     // creating variable time equaling the id of the time blocks
     var time = $(this).parent().attr("id")
 
     // setting items to local storage with the key being variable time and the value being the description added by user
     localStorage.setItem(time, description)
   });
-  
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: 
@@ -70,7 +70,23 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: 
   // How can the id attribute of each time-block be used to do this?
 
-
+  // called hour
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+  $("#hour-19 .description").val(localStorage.getItem("hour-19"));
+  $("#hour-20 .description").val(localStorage.getItem("hour-20"));
+  $("#hour-21 .description").val(localStorage.getItem("hour-21"));
+  
+  timeTrack()
 
   //
   // TODO: Add code to display the current date in the header of the page.
